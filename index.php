6b54825ef2a7bad7bd9a039fe0ea93b4e564d4d6
diego <dmnx96@gmail.com>
2024-02-21 09:28:45 +0100

    Opgaven

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="dk">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <title>Title</title>
+</head>
+<body>
+
+<div class="container">
+
+<h1>Mit navn er Diego</h1>
+
+<?php
+
+?>
+
+</div>
+
+</body>
+</html>
+
