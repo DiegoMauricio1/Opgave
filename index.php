efd8df3e1a8bd219577fbb1325b52f02a6fb6ca4
diego <dmnx96@gmail.com>
2024-02-21 09:33:56 +0100

    Opgaven

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 <body>
 
 <div class="container">
-    <h1 class="overskrift">Mit navn er Diego</h1>
+    <h1 class="overskrift">Hello World</h1>
+    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2Fearth-world-gif-earth-world-space-discover-share-gifs--636837203605902525%2F&psig=AOvVaw3Hca5qjZyr0qT0kjZJ8bFf&ust=1708590774811000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMjAwuOCvIQDFQAAAAAdAAAAABAE">
 </div>
 
 <?php
